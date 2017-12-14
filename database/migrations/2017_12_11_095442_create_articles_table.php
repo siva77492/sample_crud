de0e4ae2fbc46cb3ccb9a90fe19d54d7db4f858e
siva <siva@aasareri.com>
2017-12-14 20:57:12 +0530

    change

--- database/migrations/2017_12_11_095442_create_articles_table.php
+++ database/migrations/2017_12_11_095442_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArticalesTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateArticalesTable extends Migration
     {
         Schema::dropIfExists('articles');
 
-        
+
     }
 }
